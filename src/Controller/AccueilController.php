efd04ecef6e05ed6f883b663e7591231c9aa40d7
Marvin <Marvin@DESKTOP-PK08OIF>
2025-01-12 00:46:00 +0100

    "Phase 2 : premier test "Hello world !

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController {
+    
+    #[Route('/', name: 'accueil')]
+    public function index() : Response{
+        return new Response('Hello World');
+    }
+} 
+
